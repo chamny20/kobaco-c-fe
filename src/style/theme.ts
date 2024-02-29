88009d36c2a92045a2a932d58bb53fa3fd8c1695
chamny20 <dge3179@ajou.ac.kr>
2024-02-29 10:02:13 +0900

    style : color setting

---
+++ src/style/theme.ts
@@ -0,0 +1,15 @@
+import { DefaultTheme } from 'styled-components';
+
+export const theme: DefaultTheme = {
+  // main color
+  red_01: '#d33b4d',
+  red_02: '#dc6271',
+
+  // gray scale
+  gray_01: '#373d49',
+  gray_02: '#7b7b7b',
+  gray_03: '#909090',
+  gray_04: '#a0a0a0',
+  gray_05: '#e6e6e6',
+  gray_06: '#f8f8f8',
+};
