2bdbc06c32cc7a5831b3ec58c9e3f1d5bce59ed1
chamny20 <dge3179@ajou.ac.kr>
2024-02-29 10:02:33 +0900

    test : test defined color

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import styled, { ThemeProvider } from 'styled-components';
+import { theme } from './style/theme';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ThemeProvider theme={theme}>
+        <Temp>color theme test</Temp>
+        <p>hello world font test</p>
+      </ThemeProvider>
     </>
   );
 }
+
+const Temp = styled.div`
+  background-color: ${(props) => props.theme.red_01};
+  border: 1px solid red;
+`;
 
 export default App;
