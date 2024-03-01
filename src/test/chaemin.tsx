1db518581799e1db7a982344298c805a12ad99ee
chamny20 <chamny20@naver.com>
2024-03-02 01:36:00 +0900

    test : test button

---
+++ src/test/chaemin.tsx
@@ -0,0 +1,13 @@
+import Button from '../components/common-components/Button/Button';
+
+export const TestChaemin = () => {
+  return (
+    <div style={{ display: 'flex', gap: '10px' }}>
+      <div>hello world</div>
+      <Button>hihihi</Button>
+      <Button size="md" color="black">
+        test2
+      </Button>
+    </div>
+  );
+};
