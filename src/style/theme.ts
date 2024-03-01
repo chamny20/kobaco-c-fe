6e00ed5d0d70a6e0a028d3fbfb36497112999706
chamny20 <chamny20@naver.com>
2024-03-02 01:35:51 +0900

    style : add color

--- src/style/theme.ts
+++ src/style/theme.ts
@@ -2,10 +2,13 @@ import { DefaultTheme } from 'styled-components';
 
 export const theme: DefaultTheme = {
   // main color
+  red_active: '#B41729',
   red_01: '#d33b4d',
   red_02: '#dc6271',
+  red_03: '#F2C4CA',
 
   // gray scale
+  gray_active: '#161A22',
   gray_01: '#373d49',
   gray_02: '#7b7b7b',
   gray_03: '#909090',
