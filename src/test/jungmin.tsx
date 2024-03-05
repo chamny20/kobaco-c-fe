f70cabbbfd3c4e69e0a4e695fdfce7683f84fbef
Mungjin01 <beckaljm@naver.com>
2024-03-05 12:40:50 +0900

    feat: fulltab 컴포넌트 (#4)

--- src/test/jungmin.tsx
+++ src/test/jungmin.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
-//import RadioButton from '../components/common-components/RadioButton/RadioButton';
-import {
-  CustomTabs,
-  getTabProps,
-} from '../components/common-components/Tab/Tab';
 import Chip from '../components/common-components/Chip/Chip';
-import {
-  tabColors,
-  tabNumbers,
-  tabSizes,
-} from '../components/common-components/Tab/Tab.types';
+import FullTab from '../components/common-components/FullTab';
 
 const TestJungmin = () => {
   const tabs = [
-    getTabProps('Tab 1', tabNumbers.zero, tabSizes.medium, tabColors.red),
-    getTabProps('Tab 2', tabNumbers.one, tabSizes.medium, tabColors.gray),
+    {
+      label: '트렌드 분석',
+      content: <div>트렌드 분석 내용</div>,
+    },
+    {
+      label: '광고 아카이브',
+      content: <div>광고 아카이브 내용</div>,
+    },
+    {
+      label: '광고 만들기',
+      content: <div>광고 만들기 내용</div>,
+    },
   ];
 
   return (
@@ -22,7 +23,7 @@ const TestJungmin = () => {
       <div>라디오버튼</div>
       {/*<RadioButtons />*/}
       <div>탭</div>
-      <CustomTabs tabs={tabs} />
+      <FullTab tabs={tabs} />
       <Chip size="md" shape="rounded" color="blue">
         실험실험
       </Chip>
