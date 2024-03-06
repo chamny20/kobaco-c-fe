2d5e91519669825d5dbaadecdb09ce473b670956
chamny20 <chamny20@naver.com>
2024-03-07 00:57:31 +0900

    refactor : modified fulltab (#12)

--- src/components/common-components/FullTab.tsx
+++ src/components/common-components/FullTab.tsx
@@ -3,13 +3,21 @@ import React, { useState } from 'react';
 interface TabProps {
   label: string;
   content: React.ReactNode;
+  // size?: TapSize;
 }
 
 interface TabsProps {
   tabs: TabProps[];
+  style?: React.CSSProperties;
 }
 
-const FullTab: React.FC<TabsProps> = ({ tabs }) => {
+// export const TapSizes = {
+//   sm: 'sm',
+//   lg: 'lg',
+// } as const;
+// export type TapSize = (typeof TapSizes)[keyof typeof TapSizes];
+
+const FullTab: React.FC<TabsProps> = ({ tabs, style }) => {
   const [activeTab, setActiveTab] = useState(0);
 
   const handleTabClick = (index: number) => {
@@ -18,26 +26,27 @@ const FullTab: React.FC<TabsProps> = ({ tabs }) => {
 
   return (
     <div>
-      <div style={{ display: 'flex', gap: '20px' }}>
+      <div style={{ display: 'flex', gap: '20px', justifyContent: 'center' }}>
         {tabs.map((tab, index) => (
           <div
             key={index}
             style={{
-              padding: '9px 30px',
+              padding: '20px 50px',
+              boxSizing: 'border-box',
               background: index === activeTab ? '#D33B4D' : '#F2F2F2',
               borderRadius: '100px',
               overflow: 'hidden',
               border: `1px solid ${index === activeTab ? '#D33B4D' : '#F2F2F2'}`,
               textAlign: 'center',
               cursor: 'pointer',
+              ...style,
             }}
             onClick={() => handleTabClick(index)}
           >
             <div
               style={{
                 color: index === activeTab ? 'white' : '#A0A0A0',
-                fontSize: '14px',
-                fontFamily: 'Pretendard',
+                fontSize: '24px',
                 fontWeight: index === activeTab ? '700' : '500',
                 wordWrap: 'break-word',
               }}
@@ -52,7 +61,6 @@ const FullTab: React.FC<TabsProps> = ({ tabs }) => {
           style={{
             color: 'black',
             fontSize: '14px',
-            fontFamily: 'Pretendard',
             fontWeight: '700',
             wordWrap: 'break-word',
           }}
