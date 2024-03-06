4beb0d80a69f5f6d6ace75714a8c1a7a288e4e44
chamny20 <chamny20@naver.com>
2024-03-07 00:58:07 +0900

    feat : add router (#12)

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,13 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from '../layout/Layout';
 import { TestChaemin } from '../test/chaemin';
+import LandingPage from '../pages/landing/LandingPage';
+import TestJungmin from '../test/jungmin';
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/" element={<TestChaemin />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/test" element={<TestChaemin />} />
+          <Route path="/jj" element={<TestJungmin />} />
         </Route>
       </Routes>
     </BrowserRouter>
