27d3b013df67e835b8113ef4c864cc5181735393
chamny20 <dge3179@ajou.ac.kr>
2024-03-06 12:04:14 +0900

    feat : add modal-component (#3)

---
+++ src/components/common-components/Modal/index.ts
@@ -0,0 +1,10 @@
+import ModalMain from './Modal';
+import ModalBody from './ModalBody';
+import ModalTop from './ModalTop';
+
+export const Modal = Object.assign(ModalMain, {
+  // Title: ModalTitle,
+  // Close: ModalClose,
+  Top: ModalTop,
+  Body: ModalBody,
+});
