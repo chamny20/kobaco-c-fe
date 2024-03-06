55160549132bc5e398b8bb08a9f8ab85a0d90b9b
chamny20 <dge3179@ajou.ac.kr>
2024-03-06 15:37:09 +0900

    feat : add page and view (#12)

---
+++ src/pages/landing/LandingPage.tsx
@@ -0,0 +1,12 @@
+import { ScrollContainer } from '../../components/animation/ScrollContainer';
+import { FirstView } from './FirstView';
+
+export default function LandingPage() {
+  return (
+    <>
+      <ScrollContainer>
+        <FirstView />
+      </ScrollContainer>
+    </>
+  );
+}
