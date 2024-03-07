bcc1e62bc358bf12440ab4fed938bf0b95860540
chamny20 <dge3179@ajou.ac.kr>
2024-03-07 10:55:14 +0900

    feat : add rankcard-component in trend page (#15)

---
+++ src/components/TrendItem/RankCard.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+type RankCardProps = {
+  children?: React.ReactNode;
+};
+
+export const RankCard = ({ children }: RankCardProps) => {
+  return <RankCardContainer>{children}</RankCardContainer>;
+};
+
+export const RankCardContainer = styled.div`
+  border-radius: 12px;
+  border: 1px solid ${(props) => props.theme.gray_05};
+  background: var(--White, #fff);
+  box-shadow: 0px 0px 8px 0px rgba(0, 0, 0, 0.07);
+  padding: 20px;
+  box-sizing: border-box;
+  width: 100%;
+`;
