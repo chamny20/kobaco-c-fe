b1557bf11c36a32a2634bc5629b8a9d8d168dc33
Mungjin01 <beckaljm@naver.com>
2024-03-08 21:57:19 +0900

    feat: agechart gradient (#16)

--- src/components/chart/AgeChart.tsx
+++ src/components/chart/AgeChart.tsx
@@ -5,7 +5,6 @@ import {
   XAxis,
   Tooltip,
   Legend,
-  //CartesianGrid,
   Cell,
   LabelList,
 } from 'recharts';
@@ -19,38 +18,64 @@ const data = [
 ];
 
 const AgeChart = () => {
-  const barColor = (entry: { uv: number }) => {
+  const gradientColors = [
+    { offset: '5%', color: '#B41729', opacity: 0.8 },
+    { offset: '95%', color: '#B41729', opacity: 0 },
+    { offset: '5%', color: '#d33b4d', opacity: 0.8 },
+    { offset: '95%', color: '#d33b4d', opacity: 0 },
+    { offset: '5%', color: '#dc6271', opacity: 0.8 },
+    { offset: '95%', color: '#dc6271', opacity: 0 },
+    { offset: '5%', color: '#F2C4CA', opacity: 0.8 },
+    { offset: '95%', color: '#F2C4CA', opacity: 0 },
+  ];
+
+  const getBarFill = (entry: { uv: number }) => {
     if (entry.uv >= 15) {
-      return '#B41729';
-    } else if (entry.uv >= 10) {
-      return '#d33b4d';
+      return 'url(#gradient0)';
     } else if (entry.uv >= 5) {
-      return '#dc6271';
+      return 'url(#gradient2)';
     } else {
-      return '#F2C4CA';
+      return 'url(#gradient3)';
     }
   };
 
   return (
-    <>
-      <BarChart width={600} height={300} data={data}>
-        <XAxis dataKey="name" stroke="#474750" interval={0} />
-        <Tooltip wrapperStyle={{ width: 100, backgroundColor: '#ccc' }} />
-        <Legend width={50} />
-        {/*<CartesianGrid stroke="#ccc" strokeDasharray="5 5" />*/}
-        <Bar dataKey="uv" barSize={80} fill="#8884d8">
-          <LabelList
-            dataKey="uv"
-            position="top"
-            style={{ fill: '#000000', fontSize: 12 }}
-            formatter={(value: number) => `${value}%`}
-          />
-          {data.map((entry, index) => (
-            <Cell key={`cell-${index}`} fill={barColor(entry)} />
-          ))}
-        </Bar>
-      </BarChart>
-    </>
+    <BarChart width={600} height={300} data={data}>
+      <XAxis dataKey="name" stroke="#474750" interval={0} />
+      <Tooltip wrapperStyle={{ width: 100, backgroundColor: '#ccc' }} />
+      <Legend width={50} />
+      <Bar dataKey="uv" barSize={80}>
+        <LabelList
+          dataKey="uv"
+          position="top"
+          style={{ fill: '#000000', fontSize: 12 }}
+          formatter={(value: number) => `${value}%`}
+        />
+        {data.map((entry, index) => (
+          <Cell key={`cell-${index}`} fill={getBarFill(entry)} />
+        ))}
+      </Bar>
+
+      <defs>
+        {gradientColors.map((color, index) => (
+          <linearGradient
+            key={index}
+            id={`gradient${index}`}
+            x1="0"
+            y1="0"
+            x2="0"
+            y2="1"
+          >
+            <stop
+              offset={color.offset}
+              stopColor={color.color}
+              stopOpacity={color.opacity}
+            />
+            <stop offset="95%" stopColor={color.color} stopOpacity={0} />
+          </linearGradient>
+        ))}
+      </defs>
+    </BarChart>
   );
 };
 
