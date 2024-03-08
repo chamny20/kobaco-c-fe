30ed64a4a2cfb2dd04fb05b49944068541e73801
chamny20 <chamny20@naver.com>
2024-03-08 23:05:52 +0900

    refactor : modified similar-item (#22)

--- src/components/advertisement-detail/SimilarItem.tsx
+++ src/components/advertisement-detail/SimilarItem.tsx
@@ -1,49 +1,27 @@
 import styled from 'styled-components';
 
-// {
-//     "advertisementSimilarResponseList": [
-//       {
-//         "videoUrl": "string",
-//         "title": "string",
-//         "agency": "string",
-//         "moodInfo": [
-//           {
-//             "type": "string"
-//           }
-//         ],
-//         "isArchived": true
-//       }
-//     ]
-//   }
-
 export type SimilarItemProps = {
   videoUrl: string;
   title: string;
-  agency: string;
+  createdAt: string;
   moodInfo: string[];
   isArchived?: boolean;
-}[];
+};
 
 export const SimilarItem = ({ data }: { data: SimilarItemProps }) => {
   return (
-    <>
-      <div>비슷한 분위기의 영상</div>
-      <div>#활기찬 #역동적인</div>
-      <SimilarItemContainer>
-        {/* TODO : Mapping */}
-        <img src={videoUrl} alt="" />
-        <div className="text-box">
-          <p>{data.title}</p>
-          {/* TODO : createdAt 넣기 */}
-          <span>2020.02.02</span>
-          {/* <div>
-          {moodInfo.map((item, idx) => {
+    <SimilarItemContainer>
+      <img src={data.videoUrl} alt="" />
+      <div className="text-box">
+        <p>{data.title}</p>
+        <span>{data.createdAt}2</span>
+        <div className="tag-box">
+          {data.moodInfo.map((item, idx) => {
             return <div key={idx}>#{item}</div>;
           })}
-        </div> */}
         </div>
-      </SimilarItemContainer>
-    </>
+      </div>
+    </SimilarItemContainer>
   );
 };
 
@@ -70,6 +48,18 @@ export const SimilarItemContainer = styled.div`
     align-items: flex-start;
     border: 1px solid blue;
     width: 100%;
+
+    .tag-box {
+      display: flex;
+      gap: 13px;
+      align-items: center;
+      color: ${(props) => props.theme.gray_02};
+      font-size: 14px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: normal;
+      letter-spacing: -0.28px;
+    }
 
     p {
       color: ${(props) => props.theme.gray_01};
