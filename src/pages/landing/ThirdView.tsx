e335a1bac5bc93fa81ff7693085adf9c4ae1d2ea
chamny20 <chamny20@naver.com>
2024-03-10 00:18:54 +0900

    chore : modified style-position

--- src/pages/landing/ThirdView.tsx
+++ src/pages/landing/ThirdView.tsx
@@ -37,8 +37,8 @@ export const ThirdView = () => {
           <div className="step-box">
             {stepItems.map((item, idx) => {
               return (
-                <div key={idx}>
-                  <div className="step-item">
+                <>
+                  <div className="step-item" key={idx}>
                     <img src={item.img} alt="" />
                     <div>
                       <p>{item.title}</p>
@@ -50,7 +50,7 @@ export const ThirdView = () => {
                       <NavigateNextIcon style={{ fontSize: '48px' }} />
                     )}
                   </div>
-                </div>
+                </>
               );
             })}
           </div>
